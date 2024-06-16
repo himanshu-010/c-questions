2e3b5d29ae7e7b5a4316be6df38c39519833938c
Himasnhu Kumar <himanshukumarmpr09@gamil.com>
2024-06-16 16:15:12 +0530

    first comit

--- largestArray.cpp
+++ largestArray.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include <vector>
 #include <limits.h>
 using namespace std;
+
 int main()
 {
-    int n;
-    cin >> n;
-    int a[n];
+    int n; // variable to store the size of the array
+    cout << "Enter the size of the array: ";
+    cin >> n; // input the size of the array
+
+    vector<int> a(n); // declare a vector of size n
+
+    cout << "Enter elements of the array: ";
+    // input elements of the vector
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-    int largest = INT_MIN;
 
+    int largest = INT_MIN; // initialize largest with the smallest possible integer value
+
+    // find the largest element in the vector
     for (int i = 0; i < n; i++)
     {
         if (a[i] > largest)
@@ -19,6 +28,7 @@ int main()
             largest = a[i];
         }
     }
-    cout << largest << endl;
+
+    cout << "The largest element in the array is: " << largest << endl;
     return 0;
 }
