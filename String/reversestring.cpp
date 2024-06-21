a137aaf6b0db71a254d84c2ab4dd4b112b672607
Himasnhu Kumar <himanshukumarmpr09@gamil.com>
2024-06-21 16:51:36 +0530

    New Document

---
+++ String/reversestring.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int lenght(char input[])
+{
+    int count = 0;
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+void reverse(char input[])
+{
+    int s = 0;
+    int e = lenght(input) - 1;
+    while (s < e)
+    {
+        swap(input[s], input[e]);
+        s++;
+        e--;
+    }
+}
+
+int main()
+{
+    char name[100];
+    cout << "Enter your name : ";
+    cin.getline(name, 100);
+    cout << "Your name is : " << name << endl;
+    reverse(name);
+    cout << "Reverse of your name is : " << name << endl;
+    return 0; 
+    
+}
